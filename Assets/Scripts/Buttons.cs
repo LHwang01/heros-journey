80f95a7b9086e1b9682893c0453421e4fdeab88b
LHwang01 <LawrenceHwang01@gmail.com>
2022-03-26 20:05:55 -0400

    Created Buttons Script

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public void Story1 () {
+        SceneManager.LoadScene("Story 1");
+    }
+
+    public void Story2 () {
+        SceneManager.LoadScene("Story 2");
+    }
+
+    public void Story3 () {
+        SceneManager.LoadScene("Story 3");
+    }
+
+    public void Story4 () {
+        SceneManager.LoadScene("Story 4");
+    }
+}
