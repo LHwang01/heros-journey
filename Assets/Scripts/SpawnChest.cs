72d78122fee4a400fa10493b00e9e1e52634f212
LHwang01 <LawrenceHwang01@gmail.com>
2022-04-02 18:49:12 -0400

    Added Chest Spawn Positions

--- Assets/Scripts/SpawnChest.cs
+++ Assets/Scripts/SpawnChest.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class SpawnChest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector2[] positions = new Vector2[] {
+        new Vector2(-8.4F, -1.15F),
+        new Vector2(8.4F, -1.15F),
+        new Vector2(8.4F, 0.88F),
+        new Vector2(-8.4F, 0.88F),
+        new Vector2(8.4F, -3.5F),
+        new Vector2(-8.4F, -3.5F),
+    };
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
