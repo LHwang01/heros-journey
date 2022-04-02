e6a1ff0d33a226115bb7c59b5750bce013a0ee12
LHwang01 <LawrenceHwang01@gmail.com>
2022-04-02 19:10:33 -0400

    Finished Spawn Chest Script

--- Assets/Scripts/SpawnChest.cs
+++ Assets/Scripts/SpawnChest.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class SpawnChest : MonoBehaviour
 {
     Vector2[] positions = new Vector2[] {
-        new Vector2(-8.4F, -1.15F),
-        new Vector2(8.4F, -1.15F),
-        new Vector2(8.4F, 0.88F),
-        new Vector2(-8.4F, 0.88F),
-        new Vector2(8.4F, -3.5F),
-        new Vector2(-8.4F, -3.5F),
+        new Vector2(-8.4F, -1.2F),
+        new Vector2(8.4F, -1.2F),
+        new Vector2(8.4F, 0.8F),
+        new Vector2(-8.4F, 0.8F),
+        new Vector2(8.4F, -3.6F),
+        new Vector2(-8.4F, -3.6F),
     };
+
+    public float delayInterval = 5;
+
+    [SerializeField] GameObject chest;
 
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandChest", 1, delayInterval);
     }
 
 
     void Update()
     {
         
+    }
+
+    void SpawnRandChest()
+    {
+        Instantiate(chest, positions[Random.Range(0, positions.Length)], Quaternion.identity);
     }
 }
