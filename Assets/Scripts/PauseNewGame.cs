0eecf22a0a99967e135e9b316d38e31f9b427711
LHwang01 <LawrenceHwang01@gmail.com>
2022-04-10 18:44:02 -0400

    Added New Game Functionality

--- Assets/Scripts/PauseNewGame.cs
+++ Assets/Scripts/PauseNewGame.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pause : MonoBehaviour
+public class PauseNewGame : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject newGameMenu;
     [SerializeField] AudioSource pauseSound;
 
     void Update()
     {
         if (Input.GetKey(KeyCode.H) == true) {
+            newGameMenu.SetActive(false);
             pauseSound.Play();
             Time.timeScale = 0f;
             pauseMenu.SetActive(true);
         } else if (Input.anyKey) {
             Time.timeScale = 1f;
             pauseMenu.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.N) == true) {
+            Time.timeScale = 0f;
+            newGameMenu.SetActive(true);
         }
     }
 }
